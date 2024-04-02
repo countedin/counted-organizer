e397d4f4a3283fd51b6108f947147139909a6909
AkshayK-03 <akshaysrivathsa600@gmail.com>
2024-04-02 13:38:38 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Auth/SignUpForm/Pincode.tsx 	modified:   src/globals.css 	modified:   yarn.lock

--- src/Auth/SignUpForm/Pincode.tsx
+++ src/Auth/SignUpForm/Pincode.tsx
@@ -3,63 +3,81 @@ import TextField from "@mui/material/TextField"
 import { useState } from "react"
 import { apiSetupPin } from "../../services/BEApis/AuthAPIs"
 import { useNavigate } from "react-router"
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    '& .MuiOutlinedInput-root': {
+      '&:hover': {
+        borderColor: 'rgba(0, 0, 0, 0.87)', // Color on hover
+      },
+    },
+    '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+      borderColor: 'rgba(255, 0, 0, 1)', // Custom color when focused
+    },
+  },
+});
+
 
 const Pincode = () => {
 
   const navigate = useNavigate();
-  const [ enterPin , setEnterPin ] = useState('');
-  const [ reenterPin , setReEnterPin ] = useState('');
+  const [enterPin, setEnterPin] = useState('');
+  const [reenterPin, setReEnterPin] = useState('');
   const localAppID = localStorage.getItem('appUserId');
 
   const callSignup = async () => {
-    if(enterPin == reenterPin){
+    if (enterPin == reenterPin) {
       console.log(enterPin)
 
-      const apiRes = await apiSetupPin({ ipKeyAppUserId : localAppID , ipPin : enterPin })
+      const apiRes = await apiSetupPin({ ipKeyAppUserId: localAppID, ipPin: enterPin })
       console.log(apiRes)
       navigate('/home')
     }
-    else{
+    else {
       alert('please enter the correct pin')
     }
   }
 
+  const classes = useStyles( );
 
+  
   return (
-    // <div className="row2">
-      <div className="pincode-form">
+    
+    <div className="pincode-form">
+      <div className="signtxt">
         <h3 id="signup-txt1">Setup 4 digit PIN</h3>
         <h4 id="signup-txt2">We ask you each time when you are creating an event</h4>
-          <TextField
-            variant="outlined"
-            color="success"
-            margin="normal"
-            type="password"
-            label="Enter 4 digit PIN"
-            value={ enterPin }
-            onChange= {(e) => { setEnterPin(e.target.value) }}
-          />
-          <TextField
-            variant="outlined"
-            color="success"
-            margin="normal"
-            type="password"
-            label="Re-enter PIN"
-            value={ reenterPin }
-            onChange={(e) => { setReEnterPin(e.target.value) }}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            id="btn"
-            onClick={callSignup}
-          >
-            Finish
-          </Button>
-        
-
       </div>
+      <TextField
+        variant="outlined"
+        margin="normal"
+        type="password"
+        label="Enter 4 digit PIN"
+        value={enterPin}
+        onChange={(e) => { setEnterPin(e.target.value) }}
+        className={classes.root}
+      />
+      <TextField
+        variant="outlined"
+        // className={classes.root}
+        margin="normal"
+        type="password"
+        label="Re-enter PIN"
+        value={reenterPin}
+        onChange={(e) => { setReEnterPin(e.target.value) }}
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        id="btn"
+        onClick={callSignup}
+        style={{ textTransform: 'none', fontWeight: '600', fontSize: '15px', fontFamily: 'Poppins' }}
+      >
+        Finish
+      </Button>
+    </div>
 
   )
 }
