2286d09a4b971339d8fa2cc1ef83beaa49e23b1d
Scriptscrypt <tirumaladasa@gmail.com>
2024-06-12 11:04:39 +0530

    Feat: Past Events Tab - Revamp Design

--- src/Root/Pages/NewEvent.tsx
+++ src/Root/Pages/NewEvent.tsx
@@ -1,5 +1,4 @@
 import TextField from "@mui/material/TextField";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
--- src/Root/Pages/PastEvents.tsx
+++ src/Root/Pages/PastEvents.tsx
@@ -67,73 +67,87 @@ const PastEvents = () => {
     navigate("/exportpage");
   };
 
+  const renderRow = () => (
+    <div className="w-[440px] border border-gray-300 p-4 rounded-md hover:border-[#00c8a6]">
+      <div className="flex flex-col gap-8 w-[100%]">
+        <div className="text-[#333366] font-semibold text-xl">
+          Innovation in Semiconductor Chip Design, Fabrication, Assembly,
+          Packaging and Testing
+        </div>
+        <div className="">
+          <ul className="flex gap-4">
+            <li className="flex gap-2">
+              <CalendarMonthOutlinedIcon />
+              <div className="text-xs font-semibold">Mon, 12 Jan 2024</div>
+            </li>
+            <li className="flex gap-2">
+              <LocationOnOutlinedIcon />
+              <div className="text-xs font-semibold">
+                Dr. A.P.J Abdul Kalam Seminar Hall
+              </div>
+            </li>
+            <li className="flex gap-2">
+              <AccessTimeOutlinedIcon />
+              <div className="text-xs font-semibold">10:00am - 12:30pm</div>
+            </li>
+          </ul>
+        </div>
+
+        <Button
+          variant="contained"
+          id="attendanceButton"
+          onClick={handleButtonClick}
+          style={{
+            textTransform: "none",
+            fontWeight: "600",
+            fontSize: "15px",
+            fontFamily: "Poppins",
+          }}
+        >
+          View Atendences
+        </Button>
+      </div>
+    </div>
+  );
+
   useEffect(() => {
     fnFetchEvents();
   });
   return (
     <>
-      <div className="filterWrapper">
-        <div className="search">
-          <IconButton sx={{ p: "10px" }} aria-label="search">
-            <SearchIcon />
-          </IconButton>
-          <InputBase
-            placeholder="Search event"
-            inputProps={{ "aria-label": "eventname" }}
-            onChange={handleSearchInputChange}
-          />
-        </div>
-        <div className="filter">
-          <IconButton
-            type="button"
-            sx={{ p: "10px" }}
-            aria-label="search"
-            onClick={handleSort}
-          >
-            <SortIcon />
-          </IconButton>
-          Sort
-        </div>
-      </div>
-      <div className="tableWrapper">
-        <div className="row">
-          <div className="contentWrapper">
-            <div className="eventName">
-              Innovation in Semiconductor Chip Design, Fabrication, Assembly,
-              Packaging and Testing
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between">
+          <div className=" font-semibold text-xl">My Past Events</div>
+          <div className="flex">
+            <div className="search">
+              <IconButton sx={{ p: "10px" }} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+              <InputBase
+                placeholder="Search event"
+                inputProps={{ "aria-label": "eventname" }}
+                onChange={handleSearchInputChange}
+              />
             </div>
-            <div className="listItemWrapper">
-              <ul className="listItems">
-                <li id="listIcons">
-                  <CalendarMonthOutlinedIcon />
-                  Mon, 12 Jan 2024
-                </li>
-                <li id="listIcons">
-                  <LocationOnOutlinedIcon />
-                  Dr. A.P.J Abdul Kalam Seminar Hall
-                </li>
-                <li id="listIcons">
-                  <AccessTimeOutlinedIcon />
-                  10:00am - 12:30pm
-                </li>
-              </ul>
+            <div className="filter">
+              <IconButton
+                type="button"
+                sx={{ p: "10px" }}
+                aria-label="search"
+                onClick={handleSort}
+              >
+                <SortIcon />
+              </IconButton>
+              Sort
             </div>
           </div>
-          <div className="buttonWrapper">
-            <Button
-              variant="contained"
-              id="attendanceButton"
-              onClick={handleButtonClick}
-              style={{
-                textTransform: "none",
-                fontWeight: "600",
-                fontSize: "15px",
-                fontFamily: "Poppins",
-              }}
-            >
-              View Atendences
-            </Button>
-          </div>
+        </div>
+
+        <div className="flex flex-wrap gap-4 justify-center">
+          {renderRow()}
+          {renderRow()}
+          {renderRow()}
+          {renderRow()}
         </div>
       </div>
     </>
